6b2e1c22a6fec70ddf5b72fa568cdf2eddd549af
mustafahincal <mustafahncal@gmail.com>
2023-03-20 18:39:45 +0300

    change some style, add content to footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,6 +1,17 @@
 const Footer = () => {
   return (
-    <div className="bg-blue-200 h-16 text-black flex-shrink-0">Footer</div>
+    <div className="flex flex-row justify-between items-center py-4 px-14 bg-black text-white  border-gray-300 text-sm">
+      <div className="flex flex-row justify-between gap-3">
+        <p>Official links</p>
+        <p>Terms and Conditions</p>
+        <p>Advertising Policy</p>
+        <p>Guide for Government Agencies</p>
+        <p>Privacy Statement</p>
+      </div>
+      <div>
+        <p>© 2022 - 2023 LOGO</p>
+      </div>
+    </div>
   );
 };
 
