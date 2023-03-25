4f599e179fe91c6dc02b42800418bfdb55357d26
mustafahincal <mustafahncal@gmail.com>
2023-03-25 14:35:04 +0300

    add connect and logger utils

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
-import express from "express"
-import config from "config"
+import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+import routes from "./routes";
 
-const port = config.get<number>("port")
+const port = config.get<number>("port");
 const app = express();
 
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+  await connect();
 
-app.listen(port, () => {
-    console.log(`App is running on ${port}`)
-})
+  routes(app);
+});
--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -1,13 +1,17 @@
 import * as mongoose from "mongoose";
 import config from "config";
+import logger from "./logger";
 
-function connect() {
-    const dbUri = config.get<string>("dbUri")
+const connect = async () => {
+  const dbUri = config.get<string>("dbUri");
 
-    return mongoose.connect(dbUri).then(() => console.log("connected to db")).catch((error) =>{
-        console.error("could not connect to db")
-        process.exit(1)
-    })
-}
+  try {
+    await mongoose.connect(dbUri);
+    logger.info("DB connected");
+  } catch (error) {
+    logger.error("Could not connect to DB");
+    process.exit(1);
+  }
+};
 
-export default connect
+export default connect;
