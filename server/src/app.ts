14455078ee8d5d475f5d57e844e8f3a4df20d570
mustafahincal <mustafahncal@gmail.com>
2023-03-25 15:57:05 +0300

    use validate middlaware

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,10 +5,14 @@ import logger from "./utils/logger";
 import routes from "./routes";
 
 const port = config.get<number>("port");
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`);
+
   await connect();
 
   routes(app);
