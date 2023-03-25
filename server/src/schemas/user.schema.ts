e0ae49fd2cba4b31a708690036f5c57e0ebb58a8
mustafahincal <mustafahncal@gmail.com>
2023-03-25 15:56:44 +0300

    add user.schema for zod validation

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { object, string, TypeOf } from "zod";
+export const createUserSchema = object({
+  body: object({
+    name: string({
+      required_error: "Name is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password must be at least 6 characters"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+    email: string({
+      required_error: "Email is required",
+    }).email("Email must be valid"),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords must match",
+    path: ["passwordConfirmation"],
+  }),
+});
+
+export type CreateUserInput = TypeOf<
+  Omit<typeof createUserSchema, "body.passwordConfirmation">
+>;
