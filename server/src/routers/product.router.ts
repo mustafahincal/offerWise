560f1c96204cf2540c07ae12a1545e4162703927
mustafahincal <mustafahncal@gmail.com>
2023-03-27 16:54:26 +0300

    add productRouter

--- server/src/routers/product.router.ts
+++ server/src/routers/product.router.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getAllProductsHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "../controllers/product.controller";
 import { protect } from "../middlewares/auth.middleware";
 
 export const productRouter = express.Router();
 
-productRouter.route("/").post(protect, (req, res) => {
-  res.send(req.body);
-});
+productRouter.route("/").get(protect, getAllProductsHandler);
+productRouter.route("/:id").get(protect, getProductHandler);
+productRouter.route("/").post(protect, createProductHandler);
+productRouter.route("/:id").put(protect, updateProductHandler);
+productRouter.route("/:id").delete(protect, deleteProductHandler);
