9ced79c4b66eee70ef0b7c6738d148068963e820
mustafahincal <mustafahncal@gmail.com>
2023-03-28 14:39:19 +0300

    change layout

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,16 +1,23 @@
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 import { AuthProvider } from "@/contexts/Auth";
 import Layout from "@/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Content from "@/components/content/Content";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <div className="h-screen flex flex-col justify-between">
+        <Header />
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+        <Footer />
+      </div>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
